cd3e1793adac13e8cff4a55f170fe633d43642e4
khanisha123 <khanisha@code.edu.az>
2022-01-09 23:54:55 +0400

    cardContext

---
+++ FrontToBackForPratick/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using FrontToBackForPratick.DAL;
+using FrontToBackForPratick.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBackForPratick.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Context _context;
+
+        public HomeController(Context context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            Card slider = _context.Cards.FirstOrDefault();
+            return View();
+        }
+    }
+}
---
+++ FrontToBackForPratick/Models/Card.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBackForPratick.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+
+    }
+}
